d18e45dc330a244096e4f75caa3b79f26936562d
shashibhushan sanghani <bhushansanghani2111@gmail.com>
2022-12-19 22:03:18 +0530

    Add project files.

---
+++ QR-Code demo/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QR_Code_demo.Models;
+using QRCoder;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace QR_Code_demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string inputText)
+        {
+            using(MemoryStream ms = new MemoryStream())
+            {
+                QRCodeGenerator QrGenerator = new QRCodeGenerator();
+                QRCodeData QrData = QrGenerator.CreateQrCode(inputText, QRCodeGenerator.ECCLevel.Q);
+                QRCode obj = new QRCode(QrData);
+                using (Bitmap bitmap = obj.GetGraphic(20))
+                {
+                    bitmap.Save(ms, ImageFormat.Png);
+                    ViewBag.QrCode = "data:image/png;base64," + Convert.ToBase64String(ms.ToArray());
+                }
+            }
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ QR-Code demo/wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+function ABC() {
+    debugger
+    var divContents = document.getElementById("asldklh").innerHTML;
+    var a = window.open('', '', 'height=200, width=200');
+    a.document.write('<html>');
+    a.document.write('<body > <h1>Div contents are <br>');
+    a.document.write(divContents);
+    a.document.write('</body></html>');
+    a.document.close();
+    a.print();
+}
